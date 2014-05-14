4d5064baf1a112390e1f88e452f21afc6d667096
stevepm <steven.magelowitz@gmail.com>
2014-05-14 11:04:43 -0600

    Extra credit test passing

--- lib/center_file.rb
+++ lib/center_file.rb
@@ -1,3 +1,22 @@
+require 'text_center'
+
 class CenterFile
+  def initialize
+    @directory = File.absolute_path("../../output/", __FILE__)
+    @files = Dir.entries(File.absolute_path("../../data", __FILE__)).map do |file|
+      file if file.end_with?('.txt')
+    end.compact
+  end
+
+  def center_files
+    Dir.mkdir(@directory)
+    @files.each do |file|
+      input = File.read(File.absolute_path("../../data/#{file}", __FILE__))
+      output = TextCenter.new(input).center
+      File.open(@directory + "/center_#{file}", 'w+') do |new_file|
+        new_file.syswrite(output)
+      end
+    end
+  end
 
 end
--- lib/text_center.rb
+++ lib/text_center.rb
@@ -4,21 +4,9 @@ class TextCenter
   end
 
   def center
-    length = find_greatest_length(@input)
+    length = @input.sort_by {|line| line.length}.last.length
     @input.map do |line|
-      x = 0
-      if line.length < length
-        x = ((length-line.length)/2)
-      end
-      (line.prepend(" "*x))
+      line.center(length).rstrip
     end.join("\n")
-  end
-
-  private
-  def find_greatest_length(input)
-    length = input.map do |line|
-      line.length
-    end
-    length.sort!.last
   end
 end
