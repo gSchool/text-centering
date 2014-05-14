d12ca1a3b08e78c2cfde2bb692bdd6fbba317995
stevepm <steven.magelowitz@gmail.com>
2014-05-14 09:47:00 -0600

    Tests not passing

---
+++ lib/text_center.rb
@@ -0,0 +1,24 @@
+class TextCenter
+  def initialize(input)
+    @input = input.split("\n")
+  end
+
+  def center
+    length = find_greatest_length(@input)
+    @input.map do |line|
+      x = 0
+      if line.length < length
+        x = ((length-line.length)/2)
+      end
+      (line.prepend("*"*x) && (line + ("*")*x))
+    end
+  end
+
+  private
+  def find_greatest_length(input)
+    length = input.map do |line|
+      line.length
+    end
+    length.sort!.last
+  end
+end
